8e3dbebd6eb60014832fce89dc23ef87909f613b
Danny Foster <danny@foster.sh>
2022-12-04 16:55:06 -0600

    refactor: abstract section overlap method

--- src/2022/04/day_04.ts
+++ src/2022/04/day_04.ts
@@ -5,28 +5,50 @@ interface SectionAssignment {
   end: number;
 }
 
-const parseAssignmentPair = (pair: string): SectionAssignment[] => {
+enum Section {
+  START = 'start',
+  END = 'end',
+}
+
+const parseAssignmentPair = (pair: string) => {
   return R.pipe(
     R.split(','),
     R.map(
       R.pipe(R.split('-'), R.map(Number), ([start, end]) => ({ start, end })),
     ),
-  )(pair);
+  )(pair) as [SectionAssignment, SectionAssignment];
+};
+
+const sectionsOverlapBy = (
+  pairOrder: Section[],
+  pairOne: SectionAssignment,
+  pairTwo: SectionAssignment,
+): boolean => {
+  return R.and(
+    R.lte(pairOne[pairOrder[0]], pairTwo[pairOrder[1]]),
+    R.gte(pairOne[pairOrder[2]], pairTwo[pairOrder[3]]),
+  );
+};
+
+const checkPairsForOverlaps = (
+  pairOrder: Section[],
+  firstPair: SectionAssignment,
+  secondPair: SectionAssignment,
+): boolean => {
+  return R.or(
+    sectionsOverlapBy(pairOrder, firstPair, secondPair),
+    sectionsOverlapBy(pairOrder, secondPair, firstPair),
+  );
 };
 
 const pairFullyOverlaps = (
   firstPair: SectionAssignment,
   secondPair: SectionAssignment,
 ): boolean => {
-  return R.or(
-    R.and(
-      R.lte(firstPair.start, secondPair.start),
-      R.gte(firstPair.end, secondPair.end),
-    ),
-    R.and(
-      R.lte(secondPair.start, firstPair.start),
-      R.gte(secondPair.end, firstPair.end),
-    ),
+  return checkPairsForOverlaps(
+    [Section.START, Section.START, Section.END, Section.END],
+    firstPair,
+    secondPair,
   );
 };
 
@@ -34,15 +56,10 @@ const pairPartiallyOverlaps = (
   firstPair: SectionAssignment,
   secondPair: SectionAssignment,
 ): boolean => {
-  return R.or(
-    R.and(
-      R.lte(firstPair.start, secondPair.start),
-      R.gte(firstPair.end, secondPair.start),
-    ),
-    R.and(
-      R.lte(secondPair.start, firstPair.start),
-      R.gte(secondPair.end, firstPair.start),
-    ),
+  return checkPairsForOverlaps(
+    [Section.START, Section.START, Section.END, Section.START],
+    firstPair,
+    secondPair,
   );
 };
 
@@ -57,13 +74,11 @@ const countSectionPairOverlaps = (
     R.trim,
     R.split('\n'),
     R.reduce((totalOverlappingPairs, assignmentPair) => {
-      const [pairOne, pairTwo] = parseAssignmentPair(assignmentPair);
-
       return R.ifElse(
         ([pairOne, pairTwo]) => pairComparator(pairOne, pairTwo),
         () => R.inc(totalOverlappingPairs),
         () => totalOverlappingPairs,
-      )([pairOne, pairTwo]);
+      )([...parseAssignmentPair(assignmentPair)]);
     }, 0),
   )(input);
 };
